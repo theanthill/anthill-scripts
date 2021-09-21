e8030cfe10b63ee533d027512f65dad1e8f8ae93
robercano <3525807+robercano@users.noreply.github.com>
2021-09-21 16:05:22 +0200

    Updated auto-seigniorage for arb-rinkeby

--- scripts/trigger-seigniorage.js
+++ scripts/trigger-seigniorage.js
@@ -15,20 +15,15 @@ async function triggerSeigniorage() {
 
     try {
         const provider = new ethers.providers.JsonRpcProvider(
-            "https://data-seed-prebsc-1-s1.binance.org:8545"
+            "https://arb-rinkeby.g.alchemy.com/v2/I5TkoZm3LljH40hBPQc6a0YNbO6eo8Hy"
         );
         //const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
-        const wallet = new ethers.Wallet(
-            "ee63ac8bae6eeeb90c7d7df19e23e28a67995e0d8f1bbad10ded6d5656c5d5c1",
-            provider
-        );
+        const wallet = new ethers.Wallet("ee63ac8bae6eeeb90c7d7df19e23e28a67995e0d8f1bbad10ded6d5656c5d5c1", provider);
 
-        const { Treasury } = require("../deployments/deployments.json");
-        const treasury = new ethers.Contract(
-            Treasury.address,
-            Treasury.abi,
-            wallet
-        );
+        const deployments = require("../deployments/arb-rinkeby.deployments.json");
+        const { Treasury } = deployments.contracts;
+
+        const treasury = new ethers.Contract(Treasury.address, Treasury.abi, wallet);
 
         console.log("Allocating seigniorage...");
         const tx = await treasury.functions.allocateSeigniorage();
